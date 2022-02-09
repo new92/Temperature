264490e199d97ab4915b67e3214f708a334eb10d
new92 <94779840+new92@users.noreply.github.com>
2022-02-09 20:42:45 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,22 +1,24 @@
+#Inputs
 name=input("Please insert your name: ")
-temp=input("Please insert the temperature of the house: ")
+temp=int(input("Please insert the temperature of the house: "))
 season=input("In which season of the year are you ? [Summer - Winter]: ")
-"season" == "Summer"
-"season" == "Winter" 
-if "season" == "Summer" and "temp" <= 19 and "temp" > 10: 
+"seasons" == "Summer"
+"seasonw" == "Winter"
+#Summer
+if "seasons" == "Summer" and temp >= -10 or temp <= 9:
+    print("The temperature is in extremely low levels ! :( Consider increasing it !") 
+elif "seasons" == "Summer" and temp >= 10 or temp <= 19: 
     print("The temperature is in normal levels :) ")
-elif "season" == "Summer" and "temp" >= 20 and "temp" <= 27: 
+elif "seasons" == "Summer" and temp >= 20 or temp <= 27: 
     print("The temperature is high :( Consider lowing it down")
-elif "season" == "Summer" and "temp" > 28: 
+elif "seasons" == "Summer" and temp > 28: 
     print("Be careful !! The temperature is in extremely high levels ! Low it down !!")
-elif "season" == "Winter" and "temp" <= 19 and "temp" > 10: 
-    print("The temperature is in extremely low levels ! Consider raising it")
-elif "season" == "Winter" and "temp" >= 20 and "temp" <= 27: 
+#Winter
+elif "seasonw" == "Winter" and temp >= -10 or temp <= 9:
+    print("Be careful !! The temperature is extremely low ! Increase it !!")  
+elif "seasonw" == "Winter" and temp >= 10 or temp <= 19: 
+    print("The temperature is in low levels ! Consider increasing it")
+elif "seasonw" == "Winter" and temp >= 20 or temp <= 27: 
     print("The temperature is in normal levels :) ")
-elif "season" == "Winter" and "temp" > 28: 
+elif "seasonw" == "Winter" and temp >= 28: 
     print("The temperature is high :( Consider lowing it down")
-else:
-    while season != "Summer" and season != "Winter":
-        print("Wrong season inserted :( Please try again.")
-        season=input("Please insert season: ")
-
