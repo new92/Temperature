0262776441a7109a8d59a1611dc16c7131a5f671
new92 <94779840+new92@users.noreply.github.com>
2022-02-09 20:48:00 +0200

    Update temp.py

--- temp.py
+++ temp.py
@@ -15,9 +15,9 @@ elif "seasons" == "Summer" and temp > 28:
     print("Be careful !! The temperature is in extremely high levels ! Low it down !!")
 #Winter
 elif "seasonw" == "Winter" and temp >= -10 or temp <= 9:
-    print("Be careful !! The temperature is extremely low ! Increase it !!")  
+    print("Be careful !! The temperature is extremely low ! Increase it immediately !!")  
 elif "seasonw" == "Winter" and temp >= 10 or temp <= 19: 
-    print("The temperature is in low levels ! Consider increasing it")
+    print("The temperature is in low levels :( Consider increasing it !")
 elif "seasonw" == "Winter" and temp >= 20 or temp <= 27: 
     print("The temperature is in normal levels :) ")
 elif "seasonw" == "Winter" and temp >= 28: 
